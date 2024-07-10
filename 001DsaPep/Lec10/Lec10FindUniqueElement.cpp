795334a34803f639aaa39b003148ebdc82c1a02f
kaushik-path <85818550+kaushik-path@users.noreply.github.com>
2024-07-10 22:54:33 +0530

    Lecture 10

---
+++ 001DsaPep/Lec10/Lec10FindUniqueElement.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findUnique(int *arr, int size){
+    int ans=0;
+    for(int i=0; i<size ;i++){
+        ans= ans^arr[i];
+    }
+    return ans;
+}
+int main(){
+    int arr[] = {1,5,6,5,1};
+    findUnique(arr,5);
+    return 0;
+}

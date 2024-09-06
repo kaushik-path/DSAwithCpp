6e8b5d01513cd62da4c2054e7a0faf7e0a1f3392
Kaushik Pathak <kaushikpathak2807@gmail.com>
2024-09-06 16:37:14 +0530

    STL array

---
+++ 001DsaPep/STL/arraySTL.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdtr1c++.h>
+using namespace std;
+
+int main(){
+    
+    array<int, 5> k ={2,1,5,4,6};
+    int size = k.size();
+    for (int i = 0; i < size; i++)
+    {
+        cout<<k[i]<<endl;
+    }
+    cout<<"Element at 2nd : "<< k.at(2)<<endl; // 5
+    cout<<"empty or not : "<< k.empty()<<endl; // if empty then 0 else 1
+    cout<<"First element  : "<<k.front()<<endl;
+    cout<<"last Element : "<<k.back()<<endl;
+    return 0;
+}

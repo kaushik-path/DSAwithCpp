79d91539865160853e03dc9fc6a9de66df4012f4
Kaushik Pathak <kaushikpathak2807@gmail.com>
2024-10-21 15:42:55 +0530

    updated

--- 001DsaPep/Lec12 Binary Search/BinarySearch.cpp
+++ 001DsaPep/Lec12 Binary Search/BinarySearch.cpp
@@ -1,31 +1,29 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int binarySearch(const vector<int>& arr, int target){
+int binarySearch(const vector<int>& arr, int target) {
     int start = 0;
     int end = arr.size() - 1;
-    while(start <= end){
-        int mid = start + (end - start)/2;
-        if(arr[mid] == target){
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == target) {
             return mid;
         }
-        if(arr[mid] < target){
-            end = mid + 1;
-        }else {
-            start = mid - 1;
-            }
+        if (arr[mid] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
     }
     return -1;
 }
 
-int main()
-{
+int main() {
     vector<int> arr = {5, 9 ,10, 20, 26, 39, 42, 61};
-    int target = 9;
+    int target;
+    cout<<"enter : ";
+    cin>>target;
     int result = binarySearch(arr, target);
-
-    cout << "Target Index is : " << target;
+    cout << "Target Index is : " << result << endl;
     return 0;
-
 }

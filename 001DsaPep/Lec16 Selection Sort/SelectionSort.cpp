5b023df6a6347594a4f9b7d1fdd04534bab90830
Kaushik Pathak <kaushikpathak2807@gmail.com>
2024-11-11 16:19:51 +0530

    Selection sort

---
+++ 001DsaPep/Lec16 Selection Sort/SelectionSort.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void SelectionSort(vector<int> arr, int n) {
+    //3,9,8,6,5,4
+    for (int i = 0; i < arr.size(); i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[minIndex] > arr[j]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[minIndex], arr[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> arr = {3,9,8,6,5,4};
+    // For array >> int n = sizeof(arr) / sizeof(arr[0]);
+    // for vector
+    int n = arr.size();
+    SelectionSort(arr, n);
+
+    return 0;
+}

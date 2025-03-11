2f581e46e9f39950beb82ff2071ae02cdf539768
Kaushik Pathak <kaushikpathak2807@gmail.com>
2025-03-12 01:09:30 +0530

    String Compression

---
+++ 001DsaPep/Lec22 Char Array, Strings/09_StringCompression.cpp
@@ -0,0 +1,36 @@
+/**/
+#include<bits/stdc++.h>
+using namespace std;
+
+int compress(vector<char>& chars){
+    int i = 0;
+    int ansIndex =0;
+    int n = chars.size();
+
+    while(i<n){
+        int j=i+1;
+        while (j<n && chars[i]==chars[j]){
+            j++;
+        }
+        
+        chars[ansIndex++] = chars[i];
+        int count = j-i;
+
+        if(count>1){
+            string cnt = to_string(count);
+            for(char ch: cnt){
+                chars[ansIndex++] =ch;
+            }
+        }
+        i=j;
+    }
+    return ansIndex;
+}
+
+int main(){
+    string Str;
+    cin>>Str;
+    vector<char> Vec(Str.begin(), Str.end());
+    cout<<compress(Vec);
+    return 0;
+}
